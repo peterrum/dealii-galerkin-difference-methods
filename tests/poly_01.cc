22b0c604c4b95e3cfe57b0d75c31326142ef844c
Peter Munch <peterrmuench@gmail.com>
2024-01-10 10:06:01 +0100

    Update coeffients

--- tests/poly_01.cc
+++ tests/poly_01.cc
@@ -1,7 +1,47 @@
 #include <deal.II/base/polynomial.h>
+#include <deal.II/base/quadrature_lib.h>
 #include <deal.II/base/tensor_product_polynomials.h>
 
+#include <deal.II/fe/fe_q_base.h>
+#include <deal.II/fe/fe_values.h>
+#include <deal.II/fe/mapping_q1.h>
+
+#include <deal.II/grid/grid_generator.h>
+
 using namespace dealii;
+
+template <int dim>
+class FE_GDM : public FE_Q_Base<dim>
+{
+public:
+  FE_GDM(const ScalarPolynomialsBase<dim> &poly)
+    : FE_Q_Base<dim>(poly, create_data(poly.n()), std::vector<bool>(1, false))
+  {}
+
+  std::string
+  get_name() const override
+  {
+    return "FE_GDM";
+  }
+
+  std::unique_ptr<FiniteElement<dim>>
+  clone() const override
+  {
+    return std::make_unique<FE_GDM<dim>>(*this);
+  }
+
+private:
+  static FiniteElementData<dim>
+  create_data(const unsigned int n)
+  {
+    std::vector<unsigned int> dofs_per_object(dim + 1);
+    dofs_per_object[dim] = n;
+
+    FiniteElementData<dim> fe_data(dofs_per_object, 1, 0 /*not relevant*/);
+
+    return fe_data;
+  }
+};
 
 int
 main()
@@ -10,11 +50,13 @@ main()
   const unsigned int n_subdivisions = 20;
 
   std::vector<std::vector<double>> coefficients = {
-    {{{0., -1. / 6., 0., 1. / 6.}},
-     {{0., 1., 1. / 2., -1. / 2.}},
-     {{1., -1. / 2., -1., 1. / 2.}},
-     {{0., -1. / 3., 1. / 2., -1. / 6.}}}};
+    {{{-1.0 / 6.0, 1.0 / 2.0, -1.0 / 3.0, 0.0}},
+     {{1.0 / 2.0, -1.0, -1.0 / 2.0, 1.0}},
+     {{-1.0 / 2.0, +1.0 / 2.0, 1.0, 0.0}},
+     {{1.0 / 6.0, 0.0, -1. / 6., 0.0}}}};
 
+  for (unsigned int i = 0; i < coefficients.size(); ++i)
+    std::reverse(coefficients[i].begin(), coefficients[i].end());
 
   std::vector<Polynomials::Polynomial<double>> polynomials;
   for (unsigned int i = 0; i < coefficients.size(); ++i)
@@ -35,4 +77,30 @@ main()
         printf("%7.3f ", poly.compute_value(i, x));
       std::cout << std::endl;
     }
+
+  FE_GDM<dim> fe(poly);
+
+  MappingQ1<dim> mapping;
+  QGauss<dim>    quad(coefficients.size());
+
+  FEValues<dim> fe_values(mapping, fe, quad, update_values | update_JxW_values);
+
+  Triangulation<dim> tria;
+  GridGenerator::hyper_cube(tria);
+
+  fe_values.reinit(tria.begin());
+
+  const unsigned int dofs_per_cell = fe.n_dofs_per_cell();
+  FullMatrix<double> cell_matrix(dofs_per_cell, dofs_per_cell);
+
+  for (const unsigned int q_index : fe_values.quadrature_point_indices())
+    {
+      for (const unsigned int i : fe_values.dof_indices())
+        for (const unsigned int j : fe_values.dof_indices())
+          cell_matrix(i, j) +=
+            (fe_values.shape_value(i, q_index) *
+             fe_values.shape_value(j, q_index) * fe_values.JxW(q_index));
+    }
+
+  cell_matrix.print_formatted(std::cout);
 }
