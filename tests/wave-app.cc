a4214ad0498ded9ffba360402534416c5c27a0e2
Peter Munch <peterrmuench@gmail.com>
2024-09-26 07:33:40 +0200

    Add draft of app

---
+++ tests/wave-app.cc
@@ -0,0 +1,55 @@
+#include <deal.II/base/mpi.h>
+
+#include "wave-discretization.h"
+#include "wave-mass.h"
+#include "wave-stiffness.h"
+
+
+using namespace dealii;
+
+template <int dim, typename Number = double>
+void
+run()
+{
+  Discretization<dim, Number> discretization;
+
+  MassMatrixOperator<dim, Number>      mass_matrix_operator(discretization);
+  StiffnessMatrixOperator<dim, Number> stiffness_matrix_operator(
+    discretization);
+
+  discretization.reinit();
+
+  const std::string simulation_type = "poisson";
+
+  if (simulation_type == "poisson")
+    {
+      AssertThrow(false, ExcNotImplemented());
+    }
+  else if (simulation_type == "heat-rk")
+    {
+      AssertThrow(false, ExcNotImplemented());
+    }
+  else if (simulation_type == "heat-impl")
+    {
+      AssertThrow(false, ExcNotImplemented());
+    }
+  else if (simulation_type == "wave-rk")
+    {
+      AssertThrow(false, ExcNotImplemented());
+    }
+}
+
+int
+main(int argc, char **argv)
+{
+  Utilities::MPI::MPI_InitFinalize mpi(argc, argv, 1);
+
+  const unsigned int dim = 2;
+
+  if (dim == 1)
+    run<1>();
+  else if (dim == 2)
+    run<2>();
+  else
+    AssertThrow(false, ExcNotImplemented());
+}
