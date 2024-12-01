8020815a1106fbabb8563c5663a376973914ec57
Peter Munch <peterrmuench@gmail.com>
2024-12-01 22:01:18 +0100

    Add script for gernating coefficients

---
+++ scripts/create_coefficients.py
@@ -0,0 +1,40 @@
+import sympy as sym
+
+def print_all(expressions):
+    print("          {{\n" +",\n".join(["            {{" + ", ".join(["%.1f / %.1f" % sym.fraction(c) for c in expression.all_coeffs()]) + "}}" for expression in expressions])  + "\n          }}," )
+
+x = sym.Symbol('x')
+
+deg = 3
+start = int(deg/2)
+
+expressions = []
+
+for i in range(-deg, 1):
+    temp = 1
+
+    for j in range(0, deg + 1):
+        if i + j != 0:
+            temp *= (x+i+j)
+
+    temp = sym.Poly(temp / round(temp.evalf(subs={x: 0})))
+
+    expressions = expressions + [temp]
+
+# shift
+expressions = [sym.Poly(expressions[i].subs(x, x - (i-start)), x) for i in range(0, deg + 1)]
+
+for c in range(-start, start + 1):
+    temp = [0] * (deg + 1)
+    for cc in range(0, deg + 1):
+        for i in range(0, deg + 1):
+            factor = 1
+
+            for j in range(0, deg + 1):
+                if i!=j:
+                    factor *= (c+cc-j)/(i-j)
+
+            temp[i] += round(factor) * expressions[cc]
+
+
+    print_all(temp)
