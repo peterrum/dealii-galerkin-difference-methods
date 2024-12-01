a9e821282c32a7910e4e394ae0509c2baf2b8fed
Peter Munch <peterrmuench@gmail.com>
2024-12-01 22:07:22 +0100

    Introduce command-line argument

--- scripts/create_coefficients.py
+++ scripts/create_coefficients.py
@@ -1,40 +1,42 @@
 import sympy as sym
+import sys
 
 def print_all(expressions):
     print("          {{\n" +",\n".join(["            {{" + ", ".join(["%.1f / %.1f" % sym.fraction(c) for c in expression.all_coeffs()]) + "}}" for expression in expressions])  + "\n          }}," )
 
-x = sym.Symbol('x')
+def main():
+    x = sym.Symbol('x')
 
-deg = 3
-start = int(deg/2)
+    deg = int(sys.argv[1])
+    start = int(deg/2)
 
-expressions = []
+    expressions = []
 
-for i in range(-deg, 1):
-    temp = 1
+    for i in range(-deg, 1):
+        temp = 1
+        for j in range(0, deg + 1):
+            if i + j != 0:
+                temp *= (x+i+j)
+        temp = sym.Poly(temp / round(temp.evalf(subs={x: 0})))
+        expressions = expressions + [temp]
 
-    for j in range(0, deg + 1):
-        if i + j != 0:
-            temp *= (x+i+j)
+    # shift
+    expressions = [sym.Poly(expressions[i].subs(x, x - (i-start)), x) for i in range(0, deg + 1)]
 
-    temp = sym.Poly(temp / round(temp.evalf(subs={x: 0})))
+    for c in range(-start, start + 1):
+        temp = [0] * (deg + 1)
+        for cc in range(0, deg + 1):
+            for i in range(0, deg + 1):
+                factor = 1
 
-    expressions = expressions + [temp]
+                for j in range(0, deg + 1):
+                    if i!=j:
+                        factor *= (c+cc-j)/(i-j)
 
-# shift
-expressions = [sym.Poly(expressions[i].subs(x, x - (i-start)), x) for i in range(0, deg + 1)]
-
-for c in range(-start, start + 1):
-    temp = [0] * (deg + 1)
-    for cc in range(0, deg + 1):
-        for i in range(0, deg + 1):
-            factor = 1
-
-            for j in range(0, deg + 1):
-                if i!=j:
-                    factor *= (c+cc-j)/(i-j)
-
-            temp[i] += round(factor) * expressions[cc]
+                temp[i] += round(factor) * expressions[cc]
 
 
-    print_all(temp)
+        print_all(temp)
+
+if __name__ == '__main__':
+    main()
