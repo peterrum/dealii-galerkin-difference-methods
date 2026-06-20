4b9d6367c46c8197cbd6ee1b9aa74c62c9e71aba
Peter Munch <peterrmuench@gmail.com>
2026-06-20 08:30:28 +0200

    Make it work

--- prototypes/wave_gdm_01.cc
+++ prototypes/wave_gdm_01.cc
@@ -64,7 +64,7 @@ test()
 
         if (dim == 1)
           {
-            const auto wave_number = 1.5 * numbers::PI;
+            const auto wave_number = 1.0 * numbers::PI;
             return std::cos(wave_number * r) * std::cos(wave_number * t);
           }
         else
@@ -179,13 +179,105 @@ test()
     return result;
   };
 
+  unsigned int counter = 0;
+
+  const auto postprocess = [&](const double time, const VectorType &solution) {
+    if (true)
+      {
+        DataOutBase::VtkFlags flags;
+        flags.write_higher_order_cells = true;
+
+        DataOut<dim> data_out;
+        data_out.set_flags(flags);
+        data_out.attach_triangulation(tria);
+        data_out.add_data_vector(dof_handler, solution, "solution_lin");
+
+        data_out.set_cell_selection(
+          [&](const typename Triangulation<dim>::cell_iterator &cell) {
+            return (cell->active_cell_index() >= n_ghost_cells) &&
+                   (cell->active_cell_index() <
+                    n_subdivisions_1D + n_ghost_cells);
+          });
+
+        DoFHandler<dim> dof_handler_solution;
+        if (exact_solution)
+          {
+            exact_solution->set_time(time);
+
+            dof_handler_solution.reinit(tria);
+            dof_handler_solution.distribute_dofs(FE_Q<dim>(fe_degree));
+
+            LinearAlgebra::distributed::Vector<double> analytical_solution(
+              dof_handler_solution.n_dofs());
+            VectorTools::interpolate(mapping,
+                                     dof_handler_solution,
+                                     *exact_solution,
+                                     analytical_solution);
+            data_out.add_data_vector(dof_handler_solution,
+                                     analytical_solution,
+                                     "solution_ana");
+
+            analytical_solution = 0.0;
+
+            FEValues<dim> fe_values(
+              mapping,
+              fe,
+              dof_handler_solution.get_fe().get_unit_support_points(),
+              update_values);
+
+            for (const auto &cell : tria.active_cell_iterators())
+              if ((cell->active_cell_index() >= n_ghost_cells) &&
+                  (cell->active_cell_index() <
+                   n_subdivisions_1D + n_ghost_cells))
+                {
+                  fe_values.reinit(cell);
+
+                  std::vector<types::global_dof_index> dof_indices(
+                    fe_values.dofs_per_cell);
+
+                  for (unsigned int i = 0; i < fe_values.dofs_per_cell; ++i)
+                    dof_indices[i] =
+                      cell->active_cell_index() - n_ghost_cells + i;
+
+                  std::vector<double> quadrature_values(
+                    fe_values.n_quadrature_points);
+                  fe_values.get_function_values(solution,
+                                                dof_indices,
+                                                quadrature_values);
+
+                  cell->as_dof_handler_iterator(dof_handler_solution)
+                    ->get_dof_indices(dof_indices);
+
+                  for (unsigned int i = 0; i < dof_indices.size(); ++i)
+                    analytical_solution[dof_indices[i]] = quadrature_values[i];
+                }
+
+            data_out.add_data_vector(dof_handler_solution,
+                                     analytical_solution,
+                                     "solution");
+          }
+
+        const std::string file_name =
+          "results_" + std::to_string(counter) + ".vtu";
+        counter++;
+
+        // write data
+        data_out.build_patches(
+          mapping,
+          fe_degree,
+          DataOut<dim>::CurvedCellRegion::curved_inner_cells);
+        data_out.write_vtu_in_parallel(file_name,
+                                       dof_handler.get_mpi_communicator());
+      }
+  };
+
   // Perform time stepping
   DiscreteTime time(0, 1, delta_t);
 
   TimeStepping::ExplicitRungeKutta<BlockVectorType> rk;
   rk.initialize(TimeStepping::runge_kutta_method::RK_CLASSIC_FOURTH_ORDER);
 
-  // this->postprocess(start_t, vec_solution.block(0));
+  postprocess(0.0, solution.block(0));
 
   while ((time.is_at_end() == false))
     {
@@ -194,92 +286,10 @@ test()
                               time.get_next_step_size(),
                               solution);
 
-      // this->postprocess(time.get_current_time() +
-      //                     time.get_next_step_size(),
-      //                   vec_solution.block(0));
+      postprocess(time.get_current_time() + time.get_next_step_size(),
+                  solution.block(0));
 
       time.advance_time();
-    }
-
-  if (true)
-    {
-      DataOutBase::VtkFlags flags;
-      flags.write_higher_order_cells = true;
-
-      DataOut<dim> data_out;
-      data_out.set_flags(flags);
-      data_out.attach_triangulation(tria);
-      data_out.add_data_vector(dof_handler, solution.block(0), "solution_lin");
-
-      data_out.set_cell_selection(
-        [&](const typename Triangulation<dim>::cell_iterator &cell) {
-          return (cell->active_cell_index() >= n_ghost_cells) &&
-                 (cell->active_cell_index() <
-                  n_subdivisions_1D + n_ghost_cells);
-        });
-
-      DoFHandler<dim> dof_handler_solution;
-      if (exact_solution)
-        {
-          dof_handler_solution.reinit(tria);
-          dof_handler_solution.distribute_dofs(FE_Q<dim>(fe_degree));
-
-          LinearAlgebra::distributed::Vector<double> analytical_solution(
-            dof_handler_solution.n_dofs());
-          VectorTools::interpolate(mapping,
-                                   dof_handler_solution,
-                                   *exact_solution,
-                                   analytical_solution);
-          data_out.add_data_vector(dof_handler_solution,
-                                   analytical_solution,
-                                   "solution_ana");
-
-          analytical_solution = 0.0;
-
-          FEValues<dim> fe_values(
-            mapping,
-            fe,
-            dof_handler_solution.get_fe().get_unit_support_points(),
-            update_values);
-
-          for (const auto &cell : tria.active_cell_iterators())
-            if ((cell->active_cell_index() >= n_ghost_cells) &&
-                (cell->active_cell_index() < n_subdivisions_1D + n_ghost_cells))
-              {
-                fe_values.reinit(cell);
-
-                std::vector<types::global_dof_index> dof_indices(
-                  fe_values.dofs_per_cell);
-
-                for (unsigned int i = 0; i < fe_values.dofs_per_cell; ++i)
-                  dof_indices[i] =
-                    cell->active_cell_index() - n_ghost_cells + i;
-
-                std::vector<double> quadrature_values(
-                  fe_values.n_quadrature_points);
-                fe_values.get_function_values(solution.block(0),
-                                              dof_indices,
-                                              quadrature_values);
-
-                cell->as_dof_handler_iterator(dof_handler_solution)
-                  ->get_dof_indices(dof_indices);
-
-                for (unsigned int i = 0; i < dof_indices.size(); ++i)
-                  analytical_solution[dof_indices[i]] = quadrature_values[i];
-              }
-
-          data_out.add_data_vector(dof_handler_solution,
-                                   analytical_solution,
-                                   "solution");
-        }
-
-      const std::string file_name = "results.vtu";
-
-      // write data
-      data_out.build_patches(
-        mapping, fe_degree, DataOut<dim>::CurvedCellRegion::curved_inner_cells);
-      data_out.write_vtu_in_parallel(file_name,
-                                     dof_handler.get_mpi_communicator());
     }
 }
 
